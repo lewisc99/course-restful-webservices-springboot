24159aac3099f6603c27d2ad4e71126c9ff02aac
lewisc99 <lewiscontato99@gmail.com>
2022-09-19 12:06:25 -0300

    19. Step 07 - Creating User Bean and UserDaoService

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/dao/UserDaoService.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.restfulwebservices.dao;
+
+import com.in28minutes.rest.webservices.restfulwebservices.models.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+
+    //JPA/Hibernate > Database
+    //UserDAOService > Static List
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3,"Jim", LocalDate.now().minusYears(20)));
+
+    }
+
+    public  List<User> findAll()
+    {
+        return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
+    }
+
+}
