1f088eca85d91033545429f3e801bed72776f92f
lewisc99 <lewiscontato99@gmail.com>
2022-09-19 13:27:27 -0300

    23. Step 10 - Enhancing POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/resources/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/resources/UserResource.java
@@ -3,13 +3,16 @@ package com.in28minutes.rest.webservices.restfulwebservices.resources;
 
 import com.in28minutes.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.in28minutes.rest.webservices.restfulwebservices.models.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -36,9 +39,16 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user)
+    public ResponseEntity createUser(@RequestBody User user)
     {
-        service.save(user);
+       User savedUser =  service.save(user);
+        // /users/4 => /users /{id}, user.getID
+        //we're going to get the full url and add a path {id} that will
+        //be replaced by the id created (will be added location in the header);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
