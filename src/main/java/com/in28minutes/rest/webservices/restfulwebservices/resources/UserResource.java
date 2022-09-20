f33b5a90bcd539b7d99005872149ce097d0120a0
lewisc99 <lewiscontato99@gmail.com>
2022-09-20 07:33:12 -0300

    27. Step 13 - Implementing DELETE Method to delete a User Resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/resources/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/resources/UserResource.java
@@ -5,11 +5,7 @@ import com.in28minutes.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.in28minutes.rest.webservices.restfulwebservices.exceptions.UserNotFoundException;
 import com.in28minutes.rest.webservices.restfulwebservices.models.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
@@ -44,6 +40,7 @@ public class UserResource {
         return user;
     }
 
+
     @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user)
     {
@@ -55,6 +52,19 @@ public class UserResource {
                 .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    public void DeleteUser(@PathVariable int id)
+    {
+        User user = service.findOne(id);
+        if (user == null)
+        {
+            throw new UserNotFoundException("id: " + id);
+        }
+        service.deleteById(id);
+
     }
 
 
