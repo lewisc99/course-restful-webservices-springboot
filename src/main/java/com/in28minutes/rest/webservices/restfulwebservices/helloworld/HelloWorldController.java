e99e269d11d24c709f6c8f03d39a8e15e71fb22b
lewisc99 <lewiscontato99@gmail.com>
2022-09-21 11:34:59 -0300

    34. Step 19 - Exploring Internationalization for REST API

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,25 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 ///REST API
 @RestController
 public class HelloWorldController {
+
+
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource)
+	{
+		this.messageSource = messageSource;
+	}
 
 
 	@GetMapping(value = "/hello-world")
@@ -32,5 +44,14 @@ public class HelloWorldController {
 		return new HelloWorldBean(
 				String.format("Hello world, %s",name)
 		);
+	}
+
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized()
+	{
+
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+
 	}
 }
